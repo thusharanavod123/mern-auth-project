6f14315e7333a8647b3385f8bdedab520044598e
Thushara Navod  Padiwela <146652282+thusharanavod123@users.noreply.github.com>
2025-04-24 13:32:29 +0530

    backend models

---
+++ models/userModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verifyOtp: { type: String, default: "" },
+  verifyOypExpireAt: { type: Number, default: 0 },
+  isAccountVeried: { type: Boolean, default: false },
+  resetOtp: { type: String, default: "" },
+  resetOtpExpireAt: { type: Number, default: 0 },
+});
+
+const userModel = mongoose.models.user || mongoose.model("User", UserSchema);
+export default userModel;
