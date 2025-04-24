e730388113fa8b49d71d90f57f08062f15715f27
Thushara Navod  Padiwela <146652282+thusharanavod123@users.noreply.github.com>
2025-04-24 13:30:47 +0530

    config files

---
+++ config/mongodb.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+
+
+
+
+    mongoose.connection.on('connected', ()=>console,log('database connectd '));
+    await mongoose.connect(`${process.env.MONGODB_URI}\mern-auth`);
+
+
+
+
+
+}
+
+export default connectDB ;
