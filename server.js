71b86a13098796778d34e2531c68793ac3db1342
Thushara Navod  Padiwela <146652282+thusharanavod123@users.noreply.github.com>
2025-05-06 16:34:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv/config';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv/config";
+import cookieParser from "cookie-parser";
 
-import connectDB from './config/mongodb.js'
-import authRoutes from './routes/authRoutes.js';
+import connectDB from "./config/mongodb.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+connectDB(); // <-- Add this line
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true})); 
+app.use(cors({ credentials: true }));
 
 //API ENDPOINTS
 
-app.get('/', (req, res) => res.send('api working'));
-app.use('/api/auth', authRoutes);
+app.get("/", (req, res) => res.send("api working bor this mern-authetication "));
+app.use("/api/auth", authRoutes);
 
-
-app.get('/', (req, res) => res.send('api working hello'));
-
+app.get("/", (req, res) => res.send("api working hello"));
 
 app.listen(port, () => console.log(`Server is running on port :${port}`));
