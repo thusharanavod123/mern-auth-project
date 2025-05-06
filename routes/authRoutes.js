1a241d27306410f64b4639ce10b7bfb047d1fb84
Thushara Navod  Padiwela <146652282+thusharanavod123@users.noreply.github.com>
2025-05-06 16:32:44 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,20 @@
-import express from 'express';
-import { register } from '../controllers/authController.js';
+import express from "express";
+import { register, login, logout ,sendVerifyOtp, verifyEmail, isAuthenticated } from "../controllers/authControllers.js";
+import userAuth from "../middleware/userAuth.js";
+
+const authRouter = express.Router();
+authRouter.post("/register", register);
+authRouter.post("/login", login);
+authRouter.post("/logout", logout);
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
+authRouter.post('/send-verify-otp', userAuth, verifyEmail); 
+authRouter.post('/is-auth', userAuth, isAuthenticated); 
 
 
-const auth = express.Router();
- authRouter.post('/register', register);
-authRouter.post('/login', login);
-authRouter.get('/logout', logout);
+export default authRouter;
+
+
+
 
 
 
