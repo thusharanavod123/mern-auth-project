f1fc7a667462afedac6658b67636566e215dbc68
Thushara Navod  Padiwela <146652282+thusharanavod123@users.noreply.github.com>
2025-05-06 16:25:51 +0530

    nodemailer.js

---
+++ config/nodemailer.js
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+
+
+
+
+const transporter = nodemailer.createTransport({
+
+ 
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: process.env.SMTP_USER, // generated ethereal user
+        pass: process.env.SMTP_PASSWORD, // generated ethereal password
+    },
+
+ 
+
+});
+
+
+export default transporter;
